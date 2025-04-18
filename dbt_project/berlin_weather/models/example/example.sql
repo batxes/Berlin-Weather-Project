f3925ceb88a5b008814e0b35bfc6f499da6e6c35
batxes <batxes@gmail.com>
2025-04-18 23:54:30 +0200

    Added final changes to readme, added dockerfile so airflow can import kaggle and finished also with the dashboard in metabase

--- dbt_project/berlin_weather/models/example/example.sql
+++ dbt_project/berlin_weather/models/example/example.sql
@@ -1,17 +1,25 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
+-- models/stg_soil_temperature.sql
 
-SELECT
-  V_TE002,
-  V_TE005,
-  V_TE010,
-  V_TE020,
-  V_TE050,
-  V_TE100,
-  -- Add your transformation logic here
-  (V_TE002 + V_TE005) / 2 AS average_te002_te005
-FROM
-  `berlin-weather-project.berlin_weather_dataset.berlin_soil_temperature`
+with raw_data as (
+
+  select
+    STATIONS_ID,
+    MESS_DATUM,
+    QN_2,
+    V_TE002,
+    V_TE005,
+    V_TE010,
+    V_TE020,
+    V_TE050,
+    V_TE100,
+    eor,
+
+    -- Convert MESS_DATUM (e.g., 2023120905) to TIMESTAMP
+    parse_timestamp('%Y%m%d%H', cast(MESS_DATUM as string)) as mess_datum_ts
+
+  from `berlin-weather-project.berlin_weather_dataset.berlin_soil_temperature`
+
+)
+
+select *
+from raw_data
